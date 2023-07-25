769d703fe5896bc19cd9da1628cccb43003089a0
dado-offbrand <82716251+dado-offbrand@users.noreply.github.com>
2023-07-25 09:37:45 -0400

    Fixed via setting pulse length to not microseconds

--- ServoRangeTester.ino
+++ ServoRangeTester.ino
@@ -2,16 +2,16 @@
 #include <Adafruit_PWMServoDriver.h>
 
 /* PWM VALUE MAPPING: (target angle, min angle, max angle, pulse min, pulse max)
- * SET PWM: (servo channel, ignore and put 0, pwm value)
- * PWM ADDRESSES: 0x40, 0x70
- * POSSIBLE PWM FREQUENCIES (hz): 50, 60
- * MINUMUM PULSE WIDTH: 0
- * MAXIMUM PULSE WIDTH: 4095
+ * SET PWM ARGS: (servo channel, start delay, end time)
+ * PWM ADDRESSES: 0x40
+ * POSSIBLE PWM FREQUENCIES (hz): 60
+ * MINUMUM PULSE WIDTH: 150
+ * MAXIMUM PULSE WIDTH: 600
  */
 
 // define replaces text on compilation compared to making a new variable, saves memory
-#define SERVO_PMIN 600
-#define SERVO_PMAX 2400
+#define SERVO_PMIN 150 // 600 is MICROSECONDS
+#define SERVO_PMAX 600 // 2400 is MICROSECONDS
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40);
 
@@ -24,16 +24,16 @@ void setup() {
 }
 
 void loop() {
-  for (i = 0; i <= 180; i+=45) 
+  for (uint8_t i = 0; i <= 180; i+=45) 
   {
     pwm.setPWM(0, 0, angleToPulse(i));
     pwm.setPWM(1, 0, angleToPulse(i));
-    //pwm.setPWM(2, 0, angleToPulse(i)); testing two different servos instead of all 3 on a leg temporarily
+    //pwm.setPWM(2, 0, angleToPulse(i));
     delay(2000);
   }
 }
 
-uint16_t angleToPulse(int angle) {
+uint16_t angleToPulse(int angle) { // do not optimize to uint8_t
   uint16_t pulse = map(angle, 0, 180, SERVO_PMIN, SERVO_PMAX);
   Serial.print("angleToPulse requested, pulse: ");
   Serial.println(pulse);
