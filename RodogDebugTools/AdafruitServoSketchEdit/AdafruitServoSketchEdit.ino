fa7360f735e347b2f4db620c4a6e442b823f6aea
dado-offbrand <82716251+dado-offbrand@users.noreply.github.com>
2023-07-25 15:30:31 -0400

    Updated pulselengths

--- RodogDebugTools/AdafruitServoSketchEdit/AdafruitServoSketchEdit.ino
+++ RodogDebugTools/AdafruitServoSketchEdit/AdafruitServoSketchEdit.ino
@@ -20,24 +20,12 @@
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
-// called this way, it uses the default address 0x40
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
-// you can also call it with a different address you want
-//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
-// you can also call it with a different address and I2C interface
-//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40, Wire);
 
-// Depending on your servo make, the pulse width min and max may vary, you 
-// want these to be as small/large as possible without hitting the hard stop
-// for max range. You'll have to tweak them as necessary to match the servos you
-// have!
-#define SERVOMIN  150 // This is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX  600 // This is the 'maximum' pulse length count (out of 4096)
-#define USMIN  600 // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
-#define USMAX  2400 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
-#define SERVO_FREQ 60 // Analog servos run at ~50 Hz updates
+#define SERVOMIN  215 // This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  675 // This is the 'maximum' pulse length count (out of 4096)
+#define SERVO_FREQ 60
 
-// our servo # counter
 uint8_t servonum = 0;
 
 void setup() {
